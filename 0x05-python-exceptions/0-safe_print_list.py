f693598ebc45fb17314991d7401f87ed736ae3d8
LeboMeje <lebomhopewell@gmail.com>
2022-12-19 23:13:08 -0800

    function prints any type of variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
@@ -9,6 +8,5 @@ def safe_print_list(my_list=[], x=0):
             break
         else:
             count += 1
-
     print()
     return (count)
