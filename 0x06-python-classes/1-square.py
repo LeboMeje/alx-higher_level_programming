1642f43bb79f834e8fc2e5cab0e15d539924e298
LeboMeje <lebomhopewell@gmail.com>
2022-12-20 02:54:21 -0800

    adding a class that defines a square by its size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+class Square:
+    """Class Square that defines a square object
+    """
+    def __init__(self, size):
+        """Initialize method that stores the size of the square
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
