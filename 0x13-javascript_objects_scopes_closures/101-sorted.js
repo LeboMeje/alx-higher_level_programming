a3093d6e34e15747b27156776c63d39b4482bfbd
LeboMeje <lebomhopewell@gmail.com>
2023-03-16 01:29:25 -0700

    adding a script that computes a dictionary using another dictionary.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
 const newDict = {};
 
-Object.keys(dict).map(function (key) {
-  if (!Array.isArray(newDict[dict[key]])) {
+Object.keys(dict).map(function (key, index) {
+  if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }
   newDict[dict[key]].push(key);
