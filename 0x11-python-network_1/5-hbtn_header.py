a9d4cc7c5257d6eae14cbb8d26010888172afd04
LeboMeje <lebomhopewell@gmail.com>
2023-03-31 10:12:03 -0700

    adding a script that sends a request to the URL and displays the value of the variable X-Request-Id in the response header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,7 +9,11 @@ import sys
 
 if __name__ == "__main__":
     try:
+        if len(sys.argv) != 2:
+            raise ValueError("Usage: python3 script.py <URL>")
         r = requests.get(sys.argv[1])
+        r.raise_for_status() # Raise an exception if the request fails
         print(r.headers['X-Request-Id'])
-    try/except:
-        pass
+    except (requests.exceptions.RequestException, ValueError) as e:
+        print("Error:", e)
+        sys.exit(1)
